a754ec1bc66eb4d0e22781b052f6097c744c99b5
l.pathompong <pathompong.litk@gmail.com>
2026-03-23 14:23:34 +0700

    refactor : add user select none in title sign in page. #9

--- src/pages/SignIn/SignIn.page.jsx
+++ src/pages/SignIn/SignIn.page.jsx
@@ -3,7 +3,10 @@ import { cva } from "class-variance-authority"
 
 let style = {
     container: cva("flex flex-col gap-8 p-8 lg:w-125 *:w-full w-screen h-screen justify-center items-center"),
-    title: cva(" font-bold text-5xl text-80 text-center"),
+    title: cva(
+        "font-bold text-5xl text-80 text-center",
+        "select-none"
+    ),
 
     input: {
         container: cva(" flex flex-col *:border-2 *:border-20 *:text-40 *:focus:outline-2 *:focus:outline-20 gap-4"),
@@ -15,8 +18,7 @@ let style = {
         SignIn: cva([
             "p-4",
             "bg-60 rounded-lg text-white font-bold text-2xl ",
-            "hover:cursor-pointer",
-            "duration-300 hover:bg-80"
+            "hover:cursor-pointer duration-300 hover:bg-80"
         ])
     }
 }
